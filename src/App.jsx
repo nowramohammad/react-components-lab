5157c6e97bba9775e6a45b68d39c31219980209b
User Name <nowramohammad@Nowras-MacBook-Pro.local>
2024-11-05 22:14:58 -0600

    adding files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import WeatherForecast from './WeatherForecast';
+import './WeatherForecast.css';
+
+
 
 // src/App.jsx
 
-const App = () => {
 
+const weatherForecasts = [
+  {
+    day: 'Mon',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg',
+    imgAlt: 'sun icon',
+    conditions: 'sunny',
+    time: 'Morning',
+  },
+  {
+    day: 'Tue',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg',
+    imgAlt: 'moon icon',
+    conditions: 'clear',
+    time: 'Night',
+  },
+  {
+    day: 'Wed',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg',
+    imgAlt: 'clouds with lightning icon',
+    conditions: 'stormy',
+    time: 'All Day',
+  },
+  {
+    day: 'Thu',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg',
+    imgAlt: 'sun overcast by clouds icon',
+    conditions: 'overcast',
+    time: 'Evening',
+  },
+  {
+    day: 'Fri',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+    imgAlt: 'moon overcast by clouds icon',
+    conditions: 'cloudy',
+    time: 'Night',
+  },
+];
+
+function App() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <h1>Local Weather</h1>
+      <section> 
+        {weatherForecasts.map((forecast, index) => (
+          <WeatherForecast
+          key={index}
+          day={forecast.day}
+          img={forecast.img}
+          imgAlt={forecast.imgAlt}
+          conditions={forecast.conditions}
+          time={forecast.time}
+          />
+
+        ))}
+      </section>
+    </div>
   );
 }
+
 
 export default App
---
+++ src/WeatherIcon.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function WeatherIcon({img, imgAlt }) {
+    return <img src={img} alt={imgAlt} />;
+}
+
+export default WeatherIcon;
